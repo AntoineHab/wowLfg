fbfba73b64db77fa26a843676b37ff3ee7363b3c
AntoineHab <131660264+AntoineHab@users.noreply.github.com>
2023-09-16 00:30:38 +0200

    Initial commit

---
+++ App/Vue/header.php
@@ -0,0 +1,24 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+</head>
+<body> -->
+            <div class ="container">
+                <nav class="nav1">
+                    <ul>
+                        <li><a href="">Accueil</a></li>
+                        <li><a href="">Calendrier</a></li>
+                    </ul>
+                </nav>
+                <nav class="nav2">
+                <ul>
+                    <li><a href="">Connexion</a></li>
+                    <li><a href="https://www.twitch.tv/labiche_tv">Me Soutenir</a></li>
+                </ul>
+                </nav>
+            </div>
+<!-- </body>
+</html> -->
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+require_once './autoload.php';
+
+//Analyse de l'URL avec parse_url() et retourne ses composants
+$url = parse_url($_SERVER['REQUEST_URI']);
+//test si l'url posséde une route sinon on renvoi à la racine
+$path = isset($url['path']) ? $url['path'] : '/';
+//routeur
+switch ($path) {
+    case '/wowLfg/':
+        include './home.php';
+        break;
+    case '/wowLfg/connexion':
+        $rolesController->addRoles();
+        break;
+    case '/wowLfg/calendrier':
+        include './App/Vue/calendrier.php';
+        break;
+    default:
+        include './error.php';
+        break;
+}
+
+
+
+
+?>
