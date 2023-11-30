b9cb7e7df3857b522247dd7ea153dedd92818ef9
AntoineHab <131660264+AntoineHab@users.noreply.github.com>
2023-11-30 23:53:40 +0100

    Update header.php

--- App/Vue/header.php
+++ App/Vue/header.php
@@ -1,5 +1,4 @@
 <?php ob_start()?>
-<?php if(isset($_SESSION['connected'])):?>
     <div class ="container">
         <nav class="nav">
             <ul>
@@ -9,25 +8,14 @@
         </nav>
         <nav class="nav nav_2">
         <ul>
+            <?php if(isset($_SESSION['connected'])):?>
             <li><a href="/wowLfg/moncompte">Mon compte</a></li>
             <li><a href="/wowLfg/deconnexion">Se deconnecter</a></li>
-        </ul>
-        </nav>
-    </div>
-<?php else:?>
-    <div class ="container">
-        <nav class="nav">
-            <ul>
-                <li><a href="/wowLfg/">Accueil</a></li>
-                <li><a href="/wowLfg/calendrier">Calendrier</a></li>
-            </ul>
-        </nav>
-        <nav class="nav nav_2">
-        <ul>
+            <?php else:?>
             <li><a href="/wowLfg/connexion">Connexion</a></li>
             <li><a href="/wowLfg/inscription">Inscription</a></li>
+            <?php endif;?>
         </ul>
         </nav>
     </div>
-<?php endif;?>
 <?php $header = ob_get_clean()?>
